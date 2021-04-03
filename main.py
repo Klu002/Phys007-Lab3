0d3e612c3217abd986602767f106af503b397b21
Your Name <kevin_lu2@brown.edu>
2021-04-03 00:41:21 -0400

    changed std and avg calculations

--- main.py
+++ main.py
@@ -22,25 +22,29 @@ def get_peaks(df):
     peaks, _ = find_peaks(df.Gyroscopey, height=0)
     return df.iloc[peaks]
 
-def calculate_first_period(df):
+def calculate_period(df):
     peak_times = get_peaks(df).Time.values
     periods = []
     for i in range(len(peak_times) - 1):
         periods.append(peak_times[i + 1] - peak_times[i])
     periods = filter(lambda x: x > 0.3, periods)
     periods = list(periods)
-    return periods[2]
+    return np.average(periods[2:10]), periods[2:10]
 
 def print_stats(file):
     print("==="+file+"===")
-    long_heavy = []
+    averages = []
+    periods = []
     for i in range(1, 4):
         # Might have to change file path to /Data/
         df = get_accelerometer_info('Phys007-Lab3/Data/' + file + str(i) + '.csv')
-        long_heavy.append(calculate_first_period(df))
-    print(long_heavy)
-    print("avg: " + str(np.average(long_heavy)))
-    print("stddev: " + str(np.std(long_heavy)))
+        avg, period = calculate_period(df)
+        averages.append(avg)
+        periods = periods + period
+
+    print(averages)
+    print("avg: " + str(np.average(averages)))
+    print("stddev: " + str(np.std(periods)))
 
 if __name__ == '__main__':
     print_stats("longheavy")
